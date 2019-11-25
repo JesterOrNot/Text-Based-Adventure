55b8d1bb817b3a4538a935bebd682040d4afc963
Sean Hellum <seanhellum45@gmail.com>
2019-11-25 13:49:51 +0000

    update coloring

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@ use std::io::Write;
 fn main() {
     green!("Welcome to TextLand! Where your dreams come to life!\n");
     let name = get_adventurer_name();
-    println!("Well, Hello {}! Welcome to TextLand", name)
+    green!("Well, Hello {}! Welcome to TextLand\n", name)
 }
 fn get_adventurer_name() -> String {
-    print!("What is your name adventurer?: ");
+    cyan!("What is your name adventurer?: ");
     std::io::stdout().flush().unwrap();
     let mut adventurer_name = String::new();
     std::io::stdin().read_line(&mut adventurer_name).unwrap();
     return String::from(adventurer_name.trim());
 }
+// fn get_type() {
+//     print
+// }
